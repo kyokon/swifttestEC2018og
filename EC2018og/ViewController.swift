ce978d6ebce8b81f3245e1ebed6fa6c7949ad089
KyokoNakajima <kyokonakajima@kyokonakajimanomacbook-puro.local>
2018-07-19 19:22:53 +0900

    Initial Commit

---
+++ EC2018og/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EC2018og
+//
+//  Created by KyokoNakajima on 2018/07/19.
+//  Copyright © 2018年 KyokoNakajima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
